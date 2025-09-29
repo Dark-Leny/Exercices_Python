faa867fe1106a3eef9f37c41c11b3db4bbd0981a
Leny3000 <lenyguillard@gmail.com>
2025-09-29 15:13:19 +0000

    modif

--- exercices.py
+++ exercices.py
@@ -1,3 +1,5 @@
+# Exercice 1 : 
+
 # nom = (str(input("Entrez votre nom : ")))
 # prenom = (str(input("Entrez votre prénom : ")))
 # genre= (str(input("Veuillez préciser votre genre (H/F): ")))
@@ -11,9 +13,7 @@
 # print ("Bonjour ",titre, " ", nom[0:1].upper() + nom[1:].lower(), " ", prenom[0:1].upper() + prenom[1:].lower())
 
 
-
-
-
+# Exercice 2 : 
 
 # salaire_annuelle_brut=float(input("Saisir le salaire anuelle brut :"))
 # salaire_mensuelle_net=(0.70*salaire_annuelle_brut)/12
@@ -21,8 +21,7 @@
 # print(f"{salaire_mensuelle_net:.2f}")
 
 
-
-
+# Exercice 3 : 
 
 
 # note1 = float(input("Saississez la première note : "))
@@ -39,6 +38,7 @@
 # print(f"{moyenne:.2f}")
 
 
+# Exercice 4 : 
 
 # montant_ht = float(input("Montant HT (€) : "))
 
@@ -49,3 +49,31 @@
 #         print("Votre montant est moyen")
 #     case _:
 #         print("Votre montant est faible")
+
+
+# Exercice 5 : 
+
+# delai = int(input("Délai de paiement (en jours) : "))
+# montant = float(input("Montant de la facture (€) : "))
+
+# match delai:
+# case 0 if montant >= 500 :
+# print("Escompte validé")
+# case 0 if montant < 500 :
+# print("Escompte non validé")
+# case _:
+# print("Escompte non validé")
+
+
+# Exercice 6 : 
+
+# import math
+# N = int(input("Entrez un nombre entier posistif : "))
+# S=0
+
+# print("les N premiers termes sont :")
+# for n in range(N):
+# terme = math.pow(2,n)
+# S+= math.pow(2,n)
+# print(terme)
+# print("la somme des N premier termes est : ", S)
