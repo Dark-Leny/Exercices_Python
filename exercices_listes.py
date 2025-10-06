cd31a28056148e77930b048e334365a390e443ae
Dark-Leny <lenyguillard@gmail.com>
2025-10-06 15:45:25 +0000

    modif

--- exercices_listes.py
+++ exercices_listes.py
@@ -4,3 +4,19 @@
 
 
 # Exercice 2 : 
+
+# def unique_preserve(L):
+    
+#     result=[]
+#     for x in L:
+        
+#         if x not in result:
+            
+#             result.append(x)
+        
+#     return result 
+
+# L= unique_preserve([3,3,1,2,1,4])
+# print(L)
+# L.pop(2)
+# print(L)
