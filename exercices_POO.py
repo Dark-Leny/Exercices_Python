a11258e22cab2b3c56ac876e3c1eaca39636f757
Dark-Leny <lenyguillard@gmail.com>
2025-11-03 17:02:57 +0000

    modif

--- exercices_POO.py
+++ exercices_POO.py
@@ -3,7 +3,7 @@
 # class Lampe():
     
 #     def __init__(self, allumee):
-#         self.allumee = False
+#         self.allumee = allumee
     
 #     def allumer(self):
 #         self.allumee = True
@@ -15,7 +15,39 @@
 #         if self.allumee:
 #             print("la lampe est allumer")
 #         else:
-#             print("la lampe est étainte")
+#             print("la lampe est éteinte")
+
+
+# #utilisation
+# lampe1 = Lampe(False)
+# print(lampe1.etat())
+# lampe1.allumer()
+# print(lampe1.etat())
+# lampe1.eteindre()
+# print(lampe1.etat())
 
 
 # Exercice 2 : 
+
+# class CompteBancaire():
+
+#     def __init__(self,solde=0):
+        
+#         def deposer(self,montant):
+#             self.solde += montant
+
+#         def retirer(self,montant):
+#             if montant > self.solde:
+#                 ValueError("Le montant est supérieur au solde du compte")
+#             else:
+#                 self.solde -= montant
+
+# compte = CompteBancaire(100)
+
+# compte.deposer(50)
+# print(compte.solde)
+
+# compte.retirer(50)
+# print(compte.solde)
+
+# #compte.retirer(500)
