8f1a006a5f4ab2efc1db1c95403e4957c1ff3df2
Dark-Leny <lenyguillard@gmail.com>
2025-11-03 16:12:50 +0000

    modif

---
+++ exercices_POO.py
@@ -0,0 +1,21 @@
+# Exercice 1 : 
+
+# class Lampe():
+    
+#     def __init__(self, allumee):
+#         self.allumee = False
+    
+#     def allumer(self):
+#         self.allumee = True
+
+#     def eteindre (self):
+#         self.allumee = False
+    
+#     def etat (self):
+#         if self.allumee:
+#             print("la lampe est allumer")
+#         else:
+#             print("la lampe est étainte")
+
+
+# Exercice 2 : 
