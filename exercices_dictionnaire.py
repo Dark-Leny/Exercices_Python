709e8ca53856195bb5b24be4afbe8fbcfa174a8a
Dark-Leny <lenyguillard@gmail.com>
2025-11-10 16:57:43 +0000

    modif

--- exercices_dictionnaire.py
+++ exercices_dictionnaire.py
@@ -14,3 +14,14 @@
 # Exercice 2 : 
 
 article = {"libellé" : "céréales", "prix" : 2, "stock" : 20}
+
+article['catégorie'] = 'blé'
+
+article['prix'] = article['prix'] + 0.10 * article['prix']
+
+valeur = article.pop('stock', None)
+
+valeur = article.pop('inconnu', None)
+
+print(article)
+print(valeur)
