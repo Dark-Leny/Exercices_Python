8757f0f363398816d3e242c02ed59240e02fed1a
Dark-Leny <lenyguillard@gmail.com>
2025-11-10 16:57:06 +0000

    modif

--- article.py
+++ article.py
@@ -1,2 +1,12 @@
 article = {"libellé" : "céréales", "prix" : 2, "stock" : 20}
 
+article['catégorie'] = 'blé'
+
+article['prix'] = article['prix'] + 0.10 * article['prix']
+
+valeur = article.pop('stock', None)
+
+valeur = article.pop('inconnu', None)
+
+print(article)
+print(valeur)
