2a7a3b7b071b89d5383f14b0132d2b87385168fc
Dark-Leny <lenyguillard@gmail.com>
2025-11-10 16:24:28 +0000

    modif

---
+++ exercices_dictionnaire.py
@@ -0,0 +1,14 @@
+# Exercice 1 : 
+
+# etu = {"prenom" : "Jean", "nom" : "Dupont" , "promo": "BTS2_2025", "moyenne" : 15}
+
+# print(f"{etu["nom"].upper()} {etu["prenom"][0].upper()}{etu["prenom"][1:]} - Promo : {etu["promo"][0].upper()}{etu["promo"][1:]} - Moyenne : {etu["moyenne"]}")
+
+# age = int(input("Donnez un age : "))
+
+# etu.update({"age" : age})
+
+# print(etu)
+
+
+# Exercice 2 : 
